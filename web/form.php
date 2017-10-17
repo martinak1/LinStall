9f7698550757652d0eba8672a48092414bc5f5d9
martinak1 <martinak@mymail.vcu.edu>
2017-10-17 04:07:51 -0400

    Reverted to previous version, before combined the while loops

--- web/form.php
+++ web/form.php
@@ -97,8 +97,7 @@ function MakeTheForm($ValidationErrors) {
           <label for=\"distroUsed\" class=\"wide-label\">What distros have you used?</label>\n";
 
   // build check boxes
-  while ($distro = fgets($distroFile)) 
-  {
+  while ($distro = fgets($distroFile)) {
 
     $distro= trim($distro);
     
@@ -107,31 +106,35 @@ function MakeTheForm($ValidationErrors) {
 
     $TheForm .= "<input type=\"checkbox\" name=\"distroUsed[]\" id=\"distroUsed\" value=\"$distro\"/> $CheckedSlug $distro ";
 
-    if (isset($distroUsed) and $distroUsed != '' and in_array($distro, $distroUsed)) 
-    {
+    if (isset($distroUsed) and $distroUsed != '' and in_array($distro, $distroUsed)) {
       $CheckedSlug = 'checked';
-    } 
-    else 
-    {
+    } else {
       $CheckedSlug = '';
     }
+  }
   
   $distroFile = fopen('../distros','r');
   // BROKEN
   // favorite distro label
-    if (isset($distroNoSpaces) and $distroNoSpaces == $favDistro) 
-    {
+  $favDistro .= "</div><br>
+          <label for=\"favDistro\" class=\"form-control wide-label\">Favorite Distro\n";
+
+  // build check boxes
+  while ($distro = fgets($distroFile)) {
+
+    $distro= trim($distro);
+    
+    //Used to make id with no spaces so extract() will work 
+    $distroNoSpaces = str_replace(' ','',$distro);  
+
+    $TheForm .= "<input type=\"radio\" name=\"favDistro\" id=\"favDistro\" value=\"$distro\"> $CheckedSlug $distro ";
+
+    if (isset($distroNoSpaces) and $distroNoSpaces == $favDistro) {
       $CheckedSlug = 'checked';
     } 
-    else 
-    {
+    else {
       $CheckedSlug = '';
     }
-
-   $favDistro .= "</div><br>
-          <label for=\"favDistro\" class=\"form-control wide-label\">Favorite Distro\n
-            <input type=\"radio\" name=\"favDistro\" id=\"favDistro\" value=\"$distro\"> $CheckedSlug $distro ";
-
   }
 
   $TheForm .= "    </label></div><br><br>\n";
