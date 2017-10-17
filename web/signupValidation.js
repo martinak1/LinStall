311f61bd430ac9f403f3bc9fd48b0ab35cfda15d
martinak <martinak@mymail.vcu.edu>
2017-10-17 17:37:29 -0400

    Cleaned up the formating in signupValidation and added another test alert

--- web/signupValidation.js
+++ web/signupValidation.js
@@ -1,9 +1,11 @@
 // modified example to meet my use case
 
-function ValidateForm() {
+function ValidateForm() 
+{
 
   // verify validaiton is required
-  if (! document.signup.RunJS.checked) {
+  if (! document.signup.RunJS.checked) 
+  {
     return true;
   }
 
@@ -13,44 +15,54 @@ function ValidateForm() {
   var CrLf = "\r\n\r\n";
 
   // first name
-  if (document.signup.fName.value == '') { 
+  if (document.signup.fName.value == '') 
+  { 
     ValidationErrors += 'First name is required!' + CrLf;
   }
 
   // last name
-  if (document.signup.lName.value == '') {
+  if (document.signup.lName.value == '') 
+  {
     ValidationErrors += 'Last name is required!' + Crlf;
   }
 
   // email
-  if (document.signup.email.value == '') {
+  if (document.signup.email.value == '') 
+  {
     ValidationErrors += 'Email is required!' + CrLf;
   }
 
   // uname
-  if (document.signup.uName.value == '') {
+  if (document.signup.uName.value == '') 
+  {
     ValidationErrors += 'Username is required!' + CrLf;
   }
     
   // street
-  if (document.signup.street.value == '') {
+  if (document.signup.street.value == '') 
+  {
     ValidationErrors += 'Street address is required!' + Crlf;
   }
 
   // city
-  if (document.signup.city.value == '') {
+  if (document.signup.city.value == '') 
+  {
     ValidationErrors += 'We want to know where you live!' + Crlf;
   }
 
   // state
-  if (document.signup.state.value == '') {
+  if (document.signup.state.value == '') 
+  {
     ValidationErrors += 'We want to know what state we can find you in!' + Crlf;
   }
     
   // password
-  if ((document.signup.pass1.value == '') || (document.signup.pass2.value == '')) {
+  if ((document.signup.pass1.value == '') || (document.signup.pass2.value == '')) 
+  {
     ValidationErrors += 'Please enter your password, twice.' + CrLf;
-  } else if (document.signup.MAPass1.value != document.signup.MAPass2.value) {
+  } 
+  else if (document.signup.MAPass1.value != document.signup.MAPass2.value) 
+  {
     ValidationErrors += 'Passwords entered are not the same.' + CrLf;
   }
     
@@ -77,11 +89,16 @@ function ValidateForm() {
   {
     ValidationErrors += 'Everyone one has a story, but summarize your\'s in 50 char or less' + Crlf;
   }
+  
+  alert("I'm at the end now about to return a value");
 
   // return true if no error otherwise false
-  if (ValidationErrors == '') {
+  if (ValidationErrors == '') 
+  {
     return true;
-  } else {
+  } 
+  else 
+  {
     alert(ValidationErrors);
     return false;
   }
