962c60a0fdc7ec556401b8b5db5971a06e2c6297
martinak <martinak@mymail.vcu.edu>
2017-10-17 19:35:28 -0400

    rewrote signupValidation.js

--- web/signupValidation.js
+++ web/signupValidation.js
@@ -1,97 +1,98 @@
-// modified example to meet my use case
+// modified example to meet my use case                                       
 
-function ValidateForm() 
-{
+function ValidateForm()                                                       
+{                                                                             
 
-  // verify validaiton is required
-  if (! document.signup.RunJS.checked) 
-  {
-    return true;
-  }
+  // verify validaiton is required                                            
+  if (! document.signup.RunJS.checked)                                        
+  {                                                                           
+    return true;                                                              
+  }                                                                           
 
-  var ValidationErrors = '';
-  var CrLf = "\r\n\r\n";
+  var ValidationErrors = '';                                                  
+  var CrLf = "\r\n\r\n";                                                      
 
-  // first name
-  if (document.signup.fName.value == '') 
-  { 
-    ValidationErrors += 'First name is required!' + CrLf;
-  }
-    
-  // email
-  if (document.signup.email.value == '') 
-  {
-    ValidationErrors += 'Email is required!' + CrLf;
-  }
+  // first name                                                               
+  if (document.signup.fName.value == '')                                      
+  {                                                                           
+    ValidationErrors += 'First name is required!' + CrLf;                     
+  }                                                                           
 
-  // password
-  if ((document.signup.pass1.value == '') || (document.signup.pass2.value == '')) 
-  {
-    ValidationErrors += 'Please enter your password, twice.' + CrLf;
-  } 
-  else if (document.signup.MAPass1.value != document.signup.MAPass2.value) 
-  {
-    ValidationErrors += 'Passwords entered are not the same.' + CrLf;
-  }
 
-  // uname
-  if (document.signup.uName.value == '') 
-  {
-    ValidationErrors += 'Username is required!' + CrLf;
-  }
+  // last name                                                                
+  //if (document.signup.lName.value == '')                                    
+  //{                                                                         
+  // test alert got this far                                                  
+    //ValidationErrors += 'Last name is required!' + Crlf;                    
+  //}                                                                         
 
-    alert($ValidationErrors);
-    
-  // street
-  if (document.signup.street.value == '') 
-  {
-    ValidationErrors += 'Street address is required!' + Crlf;
-  }
+  // alert failed here                                                        
 
-  // city
-  if (document.signup.city.value == '') 
-  {
-    ValidationErrors += 'We want to know where you live!' + Crlf;
-  }
+  // email                                                                    
+  if (document.signup.email.value == '')                                      
+  {                                                                           
+    ValidationErrors += 'Email is required!' + CrLf;                          
+  }                                                                           
 
-  // state
-  if (document.signup.state.value == '') 
-  {
-    ValidationErrors += 'We want to know what state we can find you in!' + Crlf;
-  }
-    
-  // favorite distro
-  if (document.signup.favDistro.value == '')
-  {
-    ValidationErrors += 'We need to know what your favorite Linux distrobution!' + Crlf;
-  }
+  // test failed here                                                         
 
-  // distros used
-  if (document.signup.distroUsed.value == '')
-  {
-    ValidationErrors += 'Have you even used linux before?' + Crlf;
-  }
+  // password                                                                 
+  if ((document.signup.pass1.value == '') || (document.signup.pass2.value == ''))                                                                            
+  {                                                                           
+    ValidationErrors += 'Please enter your password, twice.' + CrLf;          
+  }                                                                           
+  else if (document.signup.MAPass1.value != document.signup.MAPass2.value)    
+  {                                                                           
+    ValidationErrors += 'Passwords entered are not the same.' + CrLf;         
+  }                                                                           
+        // works                                                              
 
-  // hated distro
-  if (document.signup.hatedDist.value == '')
-  {
-    ValidationErrors += 'I guess you like Tux too much to hate Linux!'+ Crlf;
-  }
+  // uname                                                                    
+  if (document.signup.uName.value == '')                                      
+  {                                                                           
+    ValidationErrors += 'Username is required!' + CrLf;                       
+  }                                                                           
 
-  // bio
-  if (document.signup.bio.value == '')
-  {
-    ValidationErrors += 'Everyone one has a story, but summarize your\'s in 50 char or less' + Crlf;
-  }
-  
-  // return true if no error otherwise false
-  if (ValidationErrors == '') 
-  {
-    return true;
-  } 
-  else 
-  {
-    alert(ValidationErrors);
-    return false;
-  }
-}
+  // street                                                                   
+  if (document.signup.street.value == '')                                     
+  {                                                                           
+    ValidationErrors += 'Street is required!' + CrLf;                         
+  }                                                                           
+
+  // city                                                                     
+  if (document.signup.city.value == '')                                       
+  {                                                                           
+    ValidationErrors += 'City is required!' + CrLf;                           
+  }                                                                           
+
+  // state                                                                    
+  if (document.signup.state.value == '')                                      
+  {                                                                           
+    ValidationErrors += 'State is required!' + CrLf;                          
+  }                                                                           
+
+  // zip                                                                      
+  if (document.signup.zip.value == '')                                        
+  {                                                                           
+    ValidationErrors += 'Zip is required!' + CrLf;                            
+  }                                                                           
+
+  // favorite distro                                                          
+  if (document.signup.favDistro.value == '')                                  
+  {                                                                           
+    ValidationErrors += 'Your favorite distro is required!' + CrLf;           
+  }                                                                           
+
+  alert(ValidationErrors);                                                    
+
+  // return true if no error otherwise false                                  
+  if (ValidationErrors == '')                                                 
+  {                                                                           
+    return true;                                                              
+  }                                                                           
+  else                                                                        
+  {                                                                           
+    alert(ValidationErrors);                                                  
+    return false;                                                             
+  }                                                                           
+}  
